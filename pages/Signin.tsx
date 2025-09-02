d19f8c5ecc79e31139523329e1c924d3b7d75372
shlomo <s0556732899@gmail.com>
2025-09-02 17:46:47 +0300

    l

--- pages/Signin.tsx
+++ pages/Signin.tsx
@@ -1,7 +1,4 @@
-import { useNavigate } from "react-router-dom";
-
 export default function Signin() {
-  const navigate = useNavigate();
   return (
     <div>
       <input type="password" placeholder="הכנס את סיסמתך למשחק" />
